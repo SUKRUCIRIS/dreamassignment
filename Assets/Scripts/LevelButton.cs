bc9dc8003528e3b4767cc7488e38e4d8455976f8
SUKRUCIRIS <sukru.ciris@arcelik.com>
2024-06-13 00:56:17 +0300

    main menu is done

---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class LevelButton : MonoBehaviour
+{
+    private const string IntDataKey = "level";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject buttonObject = GameObject.FindWithTag("mmbutton");
+        UnityEngine.UI.Button myButton = buttonObject.GetComponent<UnityEngine.UI.Button>();
+        myButton.GetComponentInChildren<TextMeshProUGUI>().text="Level "+ LoadIntData().ToString();
+        if (LoadIntData() == 10)
+        {
+            myButton.GetComponentInChildren<TextMeshProUGUI>().text = "Finished";
+        }
+        myButton.onClick.AddListener(OnButtonClick);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public static void SaveIntData(int data)
+    {
+        PlayerPrefs.SetInt(IntDataKey, data);
+        PlayerPrefs.Save();
+    }
+    public static int LoadIntData()
+    {
+        return PlayerPrefs.GetInt(IntDataKey, 1);
+    }
+    public void OnButtonClick()
+    {
+        Debug.Log("Button clicked!");
+        SceneManager.LoadScene("Game");
+    }
+}
