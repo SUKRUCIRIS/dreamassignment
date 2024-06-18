d4398cb3427c814f49558613ece3bc1062e37ce2
SUKRUCIRIS <sukru.ciris@arcelik.com>
2024-06-18 16:41:02 +0300

    build errors fixed

--- Assets/Scripts/Objects.cs
+++ Assets/Scripts/Objects.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
